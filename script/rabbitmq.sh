d4f5925111aa3b3d222630936a4134ff0c64e5f8
Bogdan Dobrelya <bogdando@mail.ru>
2019-10-01 12:37:37 +0200

    Switch to rabbitmq 3.6.15
    Signed-off-by: Bogdan Dobrelya <bogdando@mail.ru>

--- script/rabbitmq.sh
+++ script/rabbitmq.sh
@@ -6,7 +6,7 @@ wget
 # Install corosync with pacemaker
 apt-get -y install $PACKAGES
 
-ver=3.6.5
+ver=3.6.15
 file="rabbitmq-server_${ver}-1_all.deb"
 wget "http://www.rabbitmq.com/releases/rabbitmq-server/v${ver}/${file}" -O "/tmp/${file}"
 dpkg -i "/tmp/${file}"

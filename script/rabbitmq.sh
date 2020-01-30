aa8ceddad2677a4e404c9880d769f7ae10652898
Bogdan Dobrelya <bogdando@mail.ru>
2020-01-30 16:00:34 +0100

    Rework rabbitmq install
    This installs the latest one from repos (like 3.8.7 atm)
    
    Signed-off-by: Bogdan Dobrelya <bogdando@mail.ru>

--- script/rabbitmq.sh
+++ script/rabbitmq.sh
@@ -1,16 +1,6 @@
 #!/bin/bash -eux
 
-PACKAGES="
-wget
-logrotate
-"
-# Install corosync with pacemaker
-apt-get -y install $PACKAGES
-
-ver=3.6.15
-file="rabbitmq-server_${ver}-1_all.deb"
-wget "http://www.rabbitmq.com/releases/rabbitmq-server/v${ver}/${file}" -O "/tmp/${file}"
-dpkg -i "/tmp/${file}"
+apt-get -y install rabbitmq-server
 
 # stop and disable rabbitmq-server, assumes puppet CM installed
 puppet apply -e "service {'rabbitmq-server': ensure=>stopped, enable=>false }"

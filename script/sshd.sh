b782fd99a8777e71a3f15777c54bf0e611938e2e
Bogdan Dobrelya <bogdando@mail.ru>
2020-01-31 16:56:00 +0100

    Create ssh key shared for cross-nodes access
    Signed-off-by: Bogdan Dobrelya <bogdando@mail.ru>

--- script/sshd.sh
+++ script/sshd.sh
@@ -3,3 +3,7 @@ apt-get install -y openssh-server
 mkdir -p /var/run/sshd
 chmod 0755 /var/run/sshd
 echo "UseDNS no" >> /etc/ssh/sshd_config
+
+ssh-keygen -b 1024 -t rsa -f /root/.ssh/id_rsa -N "" -q
+ssh-keygen -yf ~/.ssh/id_rsa > /root/.ssh/id_rsa.pub
+cat /root/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys
